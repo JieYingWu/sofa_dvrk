bef090a4b96c2f3c2dac07419983ddc79a4fe814
JieYing Wu <jieying@jhu.edu>
2019-09-02 22:37:59 -0400

    Moved processing files to another repo

--- processing/order_points.py
+++
@@ -1,8 +0,0 @@
-# Create a canonical grid order from the initial phantom mesh before deformation
-
-import sys
-import numpy as np
-
-arr = np.loadtxt(sys.argv[1])
-ind = np.lexsort((arr[:,2], arr[:,1], arr[:,0]))
-np.savetxt('grid_order.txt',ind)
